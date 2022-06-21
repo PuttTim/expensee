27062f1c645d8fa4048f2cbe54e230cac4e51fa0
PuttTim <yannaputt@gmail.com>
2022-06-22 05:53:28 +0800

    Proof of concept: Timers
    Took 31 minutes

--- lib/screens/currency_screen.dart
+++ lib/screens/currency_screen.dart
@@ -1,13 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class CurrencyScreen extends StatelessWidget {
+class CurrencyScreen extends StatefulWidget {
   const CurrencyScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CurrencyScreen> createState() => _CurrencyScreenState();
+}
+
+class _CurrencyScreenState extends State<CurrencyScreen> {
+  String time = '';
+  Timer? timer;
+
+  @override
+  void initState() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        DateTime utc = DateTime.now().toUtc();
+        time = '${utc.hour}:${utc.minute}:${utc.second}';
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer?.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Currencies')),
-      body: const Center(child: Text('Currencies')),
-    );
+        appBar: AppBar(title: const Text('Currencies')),
+        body: Center(
+          child: Text(time),
+        ));
   }
 }
