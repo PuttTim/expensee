6e8a118c56bfb625b8296cbe0b918fbf8ff402d0
PuttTim <yannaputt@gmail.com>
2022-06-24 15:47:01 +0800

    Created TabBar navigation inside the create new transaction screen.
    Took 43 minutes

--- lib/screens/new_transaction_screen.dart
+++ lib/screens/new_transaction_screen.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:tab_indicator_styler/tab_indicator_styler.dart';
+
+import '../models/app_colours.dart';
 
 class NewTransactionScreen extends StatelessWidget {
   const NewTransactionScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Create New Transaction')),
-      body: Center(
-        child: Text("hello"),
-      ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('Create New Transaction'),
+          ),
+          body: Column(
+            children: [
+              Material(
+                color: AppColours.wittyWhite,
+                child: TabBar(
+                  indicator: MaterialIndicator(color: AppColours.moodyPurple),
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicatorWeight: 4,
+                  labelColor: AppColours.moodyPurple,
+                  indicatorColor: AppColours.moodyPurple,
+                  tabs: [
+                    Tab(text: 'Transfer'),
+                    Tab(text: 'Transaction'),
+                    Tab(text: 'Split'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Text('Transfer'),
+                    Text('Transaction'),
+                    Text('Split')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
