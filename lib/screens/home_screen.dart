5095fe809a2c2fafdaa3c42b79b1d7783e723048
PuttTim <yannaputt@gmail.com>
2022-06-27 19:32:38 +0800

    Sorting by DateTime for the Transaction Records display
    Took 6 minutes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,24 +12,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const NewRecordScreen(),
-            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const NewRecordScreen(),
           ),
-          child: const Icon(Icons.add, color: AppColours.wittyWhite),
         ),
-        appBar: AppBar(title: Text('Home')),
-        body: Container(
-            child: Consumer<RecordsProvider>(
-          builder: (context, recordsProvider, child) => ListView.separated(
-            itemBuilder: (context, index) =>
-                TransactionRecordCard(record: recordsProvider.records[index], index: index),
-            itemCount: recordsProvider.records.length,
-            separatorBuilder: (context, index) => Container(),
-          ),
-        )));
+        child: const Icon(Icons.add, color: AppColours.wittyWhite),
+      ),
+      appBar: AppBar(title: Text('Home')),
+      body: Container(
+        child: Consumer<RecordsProvider>(
+          builder: (context, recordsProvider, child) {
+            List<dynamic> records = recordsProvider.records;
+            // Sorts the transactions by DateTime, newest record first.
+            records.sort((a, b) {
+              return b.time.compareTo(a.time);
+            });
+            return ListView.separated(
+              itemBuilder: (context, index) => TransactionRecordCard(record: records[index], index: index),
+              itemCount: recordsProvider.records.length,
+              separatorBuilder: (context, index) => Container(),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
